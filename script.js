73c7ae51ee9a03bd51f57d55d88c19b573b742fa
terencechan <terence.kfc@gmail.com>
2021-09-27 14:15:32 +0800

    Completed Exercise 2

---
+++ script.js
@@ -0,0 +1,14 @@
+const hometown_btn = document.getElementById("hometown")
+const favmovies_btn = document.getElementById("favmovies")
+
+hometown_btn.addEventListener('click',()=>{
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora temporibus doloremque eos ut eligendi sequi, cumque, cum provident quae ducimus nihil voluptates excepturi magnam optio corporis quis veritatis vel ea."
+  );
+})
+
+favmovies_btn.addEventListener("click", () => {
+  alert(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora temporibus doloremque eos ut eligendi sequi, cumque, cum provident quae ducimus nihil voluptates excepturi magnam optio corporis quis veritatis vel ea."
+  );
+});
